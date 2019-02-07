19da8cd4e52e766b7f034f5aad335d35863b86d1
jelyasse1 <jelyasse1@gmail.com>
2019-02-07 17:03:27 +0000

    Récupération des champs du formulaire.

--- Inscription.java
+++ Inscription.java
@@ -16,4 +16,24 @@ public class Inscription extends HttpServlet {
     public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
         /* Traitement des données du formulaire */
     }
+
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        /* Récupération des champs du formulaire. */
+        String email = request.getParameter( CHAMP_EMAIL );
+        String motDePasse = request.getParameter( CHAMP_PASS );
+        String confirmation = request.getParameter( CHAMP_CONF );
+        String nom = request.getParameter( CHAMP_NOM );
+
+        try {
+            validationEmail( email );
+            validationMotsDePasse( motDePasse, confirmation );
+            validationNom( nom );
+        } catch (Exception e) {
+            /* Gérer les erreurs de validation ici. */
+        }
+    }
+
+    private void validationEmail( String email ) throws Exception{}
+    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{}
+    private void validationNom( String nom ) throws Exception{}
 }
